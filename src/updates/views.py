7e704c22b6c1173922c5e46a82b1f7947d293011
Ahmed Shahwan <dev.ahmed.shahwan@gmail.com>
2019-12-05 01:59:06 +0200

    SerializedDetailView and SerializedListView

--- src/updates/views.py
+++ src/updates/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.core.serializers import serialize
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
@@ -29,3 +30,24 @@ class JsonCBV2(View, JsonResponseMixin):
     def get(self, request, *args, **kwargs):
         data = {"count": 100, "content": "Some new content"}
         return self.render_to_json_response(data)
+
+
+class SerializedDetailView(View):
+    def get(self, request, *args, **kwargs):
+        obj = Update.objects.get(id=1)
+        data = serialize("json", [obj], fields=("user", "content"))
+        json_data = data
+        # data = {"user": obj.user.username, "content": obj.content}
+        # json_data = json.dumps(data)
+        return HttpResponse(json_data, content_type="application/json")
+
+
+class SerializedListView(View):
+    def get(self, request, *args, **kwargs):
+        qs = Update.objects.all()
+        data = serialize("json", qs, fields=("user", "content"))
+        json_data = data
+        print(data)
+        # data = {"user": obj.user.username, "content": obj.content}
+        # json_data = json.dumps(data)
+        return HttpResponse(json_data, content_type="application/json")
