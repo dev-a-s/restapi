4b78f615c71cd1d5f97c129461327e08943d0066
Ahmed Shahwan <dev.ahmed.shahwan@gmail.com>
2019-12-05 13:46:55 +0200

    api boilerplate code

---
+++ src/updates/api/views.py
@@ -0,0 +1,20 @@
+from updates.models import Update as UpdateModel
+
+
+class UpdateModelDetailAPIView:
+    def get(self, request, *args, **kwargs):
+        return  # json
+
+    def post(self, request, *args, **kwargs):
+        return  # json
+
+    def put(self, request, *args, **kwargs):
+        return  # json
+
+    def delete(self, request, *args, **kwargs):
+        return  # json
+
+
+class UpdateModelListAPIView:
+    def get(self, request, *args, **kwargs):
+        return  # json
